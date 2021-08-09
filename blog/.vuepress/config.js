231aad584390a7ab90364495cc89988e13c6f4db
Wang-Si-Tu <45281615+Wang-Si-Tu@users.noreply.github.com>
2021-08-09 14:30:44 +0800

    更新

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -161,7 +161,7 @@ module.exports = {
         transformer: (timestamp, lang) => {
           const moment = require("moment");
           moment.locale(lang);
-          return moment(timestamp).fromNow();
+          return moment(timestamp).format("YYYY-MM-DD HH:mm");
         }
       }
     ],
